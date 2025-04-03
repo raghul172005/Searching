32be5dee759277a2b5fd3b3663c1cb42e430be90
Raghul <163424683+raghul172005@users.noreply.github.com>
2025-04-03 18:38:12 +0530

    Create OrderAgnosticBinarySearch.java

---
+++ OrderAgnosticBinarySearch.java
@@ -0,0 +1,56 @@
+package Search1;
+import java.util.*;
+
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array element size");
+        int size=sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array elements");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the target element to search");
+        int tar=sc.nextInt();
+        int result=orderbinarySearchbinarySearch(arr,tar);
+        if(result==-1){
+            System.out.println("The target element "+tar+" not found in the array");
+        }
+        else {
+            System.out.println("The target element "+tar+" is found in the array in the location of "+(result+1));
+        }
+    }
+    public static int orderbinarySearchbinarySearch(int[] arr,int tar){
+        Arrays.sort(arr);
+        int start=0,end=arr.length-1;
+
+        while (start<=end){
+            boolean isorder=arr[start]<arr[end];
+
+            int mid=start+(end-start)/2;
+
+            if (arr[mid]==tar) {
+                return mid;
+            }
+
+            if (isorder){
+                 if (arr[mid]<tar) {
+                    start=mid+1;
+                }
+                else {
+                    end=mid-1;
+                }
+            }
+            else {
+                if (arr[mid]>tar) {
+                    start=mid-1;
+                }
+                else {
+                    end=mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
